99b608e5aca4787174ed9bde191e00114111700f
titabo2k <titabo2k@users.noreply.github.com>
2025-01-05 20:01:54 +0100

    deployment from test script

--- tests/test_prefect_flow.py
+++ tests/test_prefect_flow.py
@@ -2,6 +2,9 @@ import pytest
 import sys
 import logging
 from pathlib import Path
+from uuid import UUID
+from prefect.deployments import run_deployment
+from icecream import ic
 
 sys.path.insert(0, str(Path.joinpath(Path(__file__).parent, "../").resolve()))
 from flows.flow import greetings
@@ -10,12 +13,25 @@ logger = logging.getLogger(__name__)
 
 
 def test_flows_deployment():
-    foo = greetings.from_source(
+    foo_deployment_uuid = greetings.from_source(
         source=str(Path.joinpath(Path(__file__).parent, "../flows").resolve()),
         entrypoint="flow.py:greetings",
     ).deploy(
         name="foo_deployment",
         parameters=dict(names=["arthur", "trillian", "ford", "marvin"]),
-        work_pool_name="test_flow_pool",
+        work_pool_name="test_pool",
     )
-    logger.info(f"Deployment: {foo}")
+
+    ic(foo_deployment_uuid)
+
+    assert isinstance(foo_deployment_uuid, UUID)
+
+    # Run the deployment
+    flow_run = run_deployment(
+        name="test_flow/foo_deployment",
+        parameters=dict(names=["arthur", "trillian"]),
+    )
+
+    ic(flow_run)
+
+    assert foo_deployment_uuid == flow_run.deployment_id
